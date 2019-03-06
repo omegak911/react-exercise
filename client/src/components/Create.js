2d027e3e676f2608e346ce0c2d4c8f0b39cdbf08
Kin Chan <xkinchanx@gmail.com>
2019-03-06 12:28:26 -0800

    [prog] create js template

---
+++ client/src/components/Create.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Create = () =>
+  <div className="center">
+    <form action="" >
+      <input type="text"/>
+      <button type="submit">add</button>
+    </form>
+  </div>
+
+export default Create;
