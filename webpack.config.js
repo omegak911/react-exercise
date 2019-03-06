10f207bcbfe516b9c92762f65f0cb5d22f6ef422
Kin Chan <xkinchanx@gmail.com>
2019-03-06 10:57:56 -0800

    [ops] dependencies and webpack setup.  trying to keep it modular, might be missing things

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react', 'stage-0']
+        }
+      }
+    ]
+  }
+};
