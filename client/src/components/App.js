561212dd580edf1e988271252530b736b48d6476
Kin Chan <xkinchanx@gmail.com>
2019-03-06 12:25:15 -0800

    [prog] base code with update example

---
+++ client/src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import List from './List';
+import Create from './Create';
+import ListItem from './ListItem';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      mostLiked: { likes: 0 }
+    }
+  }
+
+  /*TODO
+
+  add item: which creates a new item in the state items array
+    - format needs to be { name: name, likes: 0}
+
+  delete item: removes one of the clicked items from the array
+  */
+
+  //accepts and index, will increase like count for that item and also determine who has the most likes
+  updateItem = (index) => {  
+    let items = [...this.state.items];
+    let { mostLiked } = this.state;
+    let itemToUpdate = items[index];
+    
+    itemToUpdate.likes += 1;
+    mostLiked = mostLiked.likes >= itemToUpdate.likes ? mostLiked : itemToUpdate;
+    
+    this.setState({ items, mostLiked });
+  }
+
+  render() {
+  return (
+    <div id="mainApp" className="center vertical">
+      {mostLiked.likes > 0 && <div id="mostLiked"><div>Most Liked:</div><ListItem number={'hey'} item={mostLiked}/></div>}
+      <div>SHOULD DISPLAY CREATE</div>
+      <div>SHOULD DISPLAY LIST</div>
+    </div>
+    )
+  }
+}
